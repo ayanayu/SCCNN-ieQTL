7d6e4d184ab59ba182325171f3ec2d7d7436d6cc
ayanayu <68268861+ayanayu@users.noreply.github.com>
2022-01-17 11:52:36 +0800

    Delete Program_CNN_input_vcf_prepare directory

--- Program_CNN_input_vcf_prepare/main_4.sh
+++
@@ -1,9 +0,0 @@
-
-###### The script is used to transform hg38 to hg19 bed 
-
-# define paths
-vcf_directory=/media/ggj/My\ Passport1/Xiaoyanyu/CNN/vcf/OtherTissue/ 
-coordinate_file=/home/ggj/hg38ToHg19.over.chain.gz
-
-mkdir ${vcf_directory}h19
-liftOver ${vcf_directory}unique_pos_bed.txt $coordinate_file ${vcf_directory}h19/unique_pos_bed.txt ${vcf_directory}h19/'NotMap'
